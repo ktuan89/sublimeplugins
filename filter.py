5ca18626d367e2bfb6e8375954bb8c6e7a6cf836
Anh Tuan Khuc <anhk@uber.com>
2016-09-20 20:43:45 -0700

    Add explanation for dependencies + convert tabs -> spaces

--- filter.py
+++ filter.py
@@ -1,9 +1,9 @@
 import sublime, sublime_plugin
 
 class FilterText(sublime_plugin.TextCommand):
-	def run(self, edit):
-		window = self.view.window()
-		window.show_input_panel(
+    def run(self, edit):
+        window = self.view.window()
+        window.show_input_panel(
             'Filter for:',
             '',
             lambda query: (
@@ -12,18 +12,19 @@ class FilterText(sublime_plugin.TextCommand):
             None,
             None
         )
-		pass
+        pass
 
-	def filter(self, edit, text):
-		negative = False
-		if text.startswith("-"):
-			text = text[1:]
-			negative = True
-		str = self.view.substr(sublime.Region(0, self.view.size()))
-		lines = str.split('\n')
-		new_lines = []
-		for line in lines:
-			if (line.find(text) > -1 and not negative) or (negative and line.find(text) == -1):
-				new_lines.append(line)
-		self.view.run_command('replace_content', {"new_content": '\n'.join(new_lines)})
-		pass
+    def filter(self, edit, text):
+        negative = False
+        if text.startswith("-"):
+            text = text[1:]
+            negative = True
+        str = self.view.substr(sublime.Region(0, self.view.size()))
+        lines = str.split('\n')
+        new_lines = []
+        for line in lines:
+            if (line.find(text) > -1 and not negative) or (negative and line.find(text) == -1):
+                new_lines.append(line)
+        # deps: this is from utilities.py
+        self.view.run_command('replace_content', {"new_content": '\n'.join(new_lines)})
+        pass
--- kt_git.py
+++ kt_git.py
@@ -5,22 +5,22 @@ import codecs
 
 def wait_for_view_to_be_loaded_then_do(view, func):
 
-	def wait_for_view_to_be_loaded_then_do_exp(view, func, timeout):
-		if view.is_loading():
-			sublime.set_timeout(lambda: wait_for_view_to_be_loaded_then_do(view, func), timeout * 2)
-			return
-		func()
+    def wait_for_view_to_be_loaded_then_do_exp(view, func, timeout):
+        if view.is_loading():
+            sublime.set_timeout(lambda: wait_for_view_to_be_loaded_then_do(view, func), timeout * 2)
+            return
+        func()
 
-	wait_for_view_to_be_loaded_then_do_exp(view, func, 10)
+    wait_for_view_to_be_loaded_then_do_exp(view, func, 10)
 
 def gitSettings():
-	return sublime.load_settings('Git.sublime-settings')
+    return sublime.load_settings('Git.sublime-settings')
 
 def gitPath():
-	return gitSettings().get('path')
+    return gitSettings().get('path')
 
 def tmpFile():
-	return gitSettings().get('tmp_file')
+    return gitSettings().get('tmp_file')
 
 class GitBase(sublime_plugin.WindowCommand):
     def gitCommand(self):
@@ -45,6 +45,7 @@ class GitBase(sublime_plugin.WindowCommand):
 
             results_view.set_name(self.gitName())
 
+            # deps: this is from utilities.py
             results_view.run_command('replace_content', {"new_content": stdout})
             results_view.sel().clear()
             results_view.sel().add(sublime.Region(0, 0))
@@ -71,146 +72,147 @@ class GitDiff(GitBase):
         return "GitDiff"
 
 class GitDiffOpen(sublime_plugin.WindowCommand):
-	def positionFromView(self, view):
-		for region in view.sel():
-			if region.empty():
-				return region.begin()
-		return -1
-	def run(self):
-		view = self.window.active_view()
-		if view.name().startswith("Git") or (view.file_name() is not None and view.file_name().endswith("diff")):
-			print("ere")
-			pos = self.positionFromView(view)
-			line = view.line(pos)
+    def positionFromView(self, view):
+        for region in view.sel():
+            if region.empty():
+                return region.begin()
+        return -1
+    def run(self):
+        view = self.window.active_view()
+        if view.name().startswith("Git") or (view.file_name() is not None and view.file_name().endswith("diff")):
+            print("ere")
+            pos = self.positionFromView(view)
+            line = view.line(pos)
 
-			current_line = view.substr(line)
+            current_line = view.substr(line)
 
 
-			line_offset = pos - line.begin()
-			position_str = ""
-			file_str = ""
-			line_count = 0
-			while True:
-				if line.begin() <= 0:
-					break
-				line = view.line(line.begin() - 1)
-				str = view.substr(line)
+            line_offset = pos - line.begin()
+            position_str = ""
+            file_str = ""
+            line_count = 0
+            while True:
+                if line.begin() <= 0:
+                    break
+                line = view.line(line.begin() - 1)
+                str = view.substr(line)
 
-				if position_str=="" and not str.startswith("-"):
-					line_count = line_count + 1
+                if position_str=="" and not str.startswith("-"):
+                    line_count = line_count + 1
 
-				if position_str=="" and str.startswith("@@"):
-					position_str = str
+                if position_str=="" and str.startswith("@@"):
+                    position_str = str
 
-				if file_str=="" and str.startswith("+++"):
-					file_str = str
+                if file_str=="" and str.startswith("+++"):
+                    file_str = str
 
-				if file_str != "" and position_str != "":
-					break
+                if file_str != "" and position_str != "":
+                    break
 
-			if file_str == "" or position_str == "":
-				return
+            if file_str == "" or position_str == "":
+                return
 
-			file_name = file_str[len("+++ b/"):]
-			matches = re.search('@@.*\+(.*),(.*) @@', position_str)
-			extract_position = (int(matches.group(1)) - 1) + (line_count - 1)
+            file_name = file_str[len("+++ b/"):]
+            matches = re.search('@@.*\+(.*),(.*) @@', position_str)
+            extract_position = (int(matches.group(1)) - 1) + (line_count - 1)
 
-			new_view = self.window.open_file(gitPath() + file_name)
+            new_view = self.window.open_file(gitPath() + file_name)
 
-			def handle_view():
-				new_position = new_view.text_point(extract_position, line_offset)
-				new_view.show_at_center(new_position)
-				new_view.sel().clear()
-				new_view.sel().add(sublime.Region(new_position, new_position))
+            def handle_view():
+                new_position = new_view.text_point(extract_position, line_offset)
+                new_view.show_at_center(new_position)
+                new_view.sel().clear()
+                new_view.sel().add(sublime.Region(new_position, new_position))
 
-			wait_for_view_to_be_loaded_then_do(new_view, handle_view)
-			pass # happy dancing
-		elif view.name().startswith("grep:"):
-			self.window.run_command('open', {"skip_dialog": True})
-			pass
-		pass
+            wait_for_view_to_be_loaded_then_do(new_view, handle_view)
+            pass # happy dancing
+        elif view.name().startswith("grep:"):
+            # deps: going to add this fallback soon
+            self.window.run_command('open', {"skip_dialog": True})
+            pass
+        pass
 
 class GitListModifiedFiles(sublime_plugin.WindowCommand):
 
-	files = []
+    files = []
 
-	def run(self):
-		command = "cd {0};git status".format(gitPath())
-		p = Popen(command, shell=True, close_fds=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    def run(self):
+        command = "cd {0};git status".format(gitPath())
+        p = Popen(command, shell=True, close_fds=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
-		p.wait()
-		files = []
+        p.wait()
+        files = []
 
-		for line in p.stdout.readlines():
-			str = line.decode("utf-8")
-			print(str)
-			matches = re.search('both modified:\s+((((\w|-)+)\/)*(\w+)\.(\w+))', str)
-			if matches is not None:
-				file_name = matches.group(1)
-				files.append(file_name)
+        for line in p.stdout.readlines():
+            str = line.decode("utf-8")
+            print(str)
+            matches = re.search('both modified:\s+((((\w|-)+)\/)*(\w+)\.(\w+))', str)
+            if matches is not None:
+                file_name = matches.group(1)
+                files.append(file_name)
 
-		self.files = files
-		print(files)
+        self.files = files
+        print(files)
 
-		self.window.show_quick_panel(self.files, self.tab_selected)
+        self.window.show_quick_panel(self.files, self.tab_selected)
 
-		pass
+        pass
 
-	def tab_selected(self, selected):
-		if selected > -1:
-			file_name = self.files[selected]
-			self.window.open_file(gitPath() + file_name)
-			pass
+    def tab_selected(self, selected):
+        if selected > -1:
+            file_name = self.files[selected]
+            self.window.open_file(gitPath() + file_name)
+            pass
 
 class GitAdd(sublime_plugin.WindowCommand):
 
-	files = []
+    files = []
 
-	def run(self):
-		file_name = self.window.active_view().file_name()
-		if file_name is None:
-			return
-		sublime.status_message("git: add ...")
-		command = ("cd {0};git add " + file_name).format(gitPath())
-		p = Popen(command, shell=True, close_fds=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-		p.wait()
-		sublime.status_message("git: added")
-		for line in p.stdout.readlines():
-			str = line.decode("utf-8")
-			print(str)
-		pass
+    def run(self):
+        file_name = self.window.active_view().file_name()
+        if file_name is None:
+            return
+        sublime.status_message("git: add ...")
+        command = ("cd {0};git add " + file_name).format(gitPath())
+        p = Popen(command, shell=True, close_fds=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        p.wait()
+        sublime.status_message("git: added")
+        for line in p.stdout.readlines():
+            str = line.decode("utf-8")
+            print(str)
+        pass
 
 class GitListBranches(sublime_plugin.WindowCommand):
 
-	branches = []
+    branches = []
 
-	def run(self):
-		command = "cd {0};git branch".format(gitPath())
-		p = Popen(command, shell=True, close_fds=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    def run(self):
+        command = "cd {0};git branch".format(gitPath())
+        p = Popen(command, shell=True, close_fds=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
-		p.wait()
-		branches = []
-		display_branches = []
+        p.wait()
+        branches = []
+        display_branches = []
 
-		for line in p.stdout.readlines():
-			str = line.decode("utf-8")
-			matches = re.search('(\*|\s)+(.*)', str)
-			if matches is not None:
-				branch = matches.group(2)
-				branches.append(branch)
-				display_branches.append(str)
+        for line in p.stdout.readlines():
+            str = line.decode("utf-8")
+            matches = re.search('(\*|\s)+(.*)', str)
+            if matches is not None:
+                branch = matches.group(2)
+                branches.append(branch)
+                display_branches.append(str)
 
-		self.branches = branches
+        self.branches = branches
 
-		self.window.show_quick_panel(display_branches, self.tab_selected)
+        self.window.show_quick_panel(display_branches, self.tab_selected)
 
-		pass
+        pass
 
-	def tab_selected(self, selected):
-		if selected > -1:
-			branch = self.branches[selected]
-			command = ("cd {0};git checkout " + branch).format(gitPath())
-			p = Popen(command, shell=True, close_fds=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-			p.wait()
-			sublime.status_message("git: checkout " + branch)
-			pass
+    def tab_selected(self, selected):
+        if selected > -1:
+            branch = self.branches[selected]
+            command = ("cd {0};git checkout " + branch).format(gitPath())
+            p = Popen(command, shell=True, close_fds=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            p.wait()
+            sublime.status_message("git: checkout " + branch)
+            pass
