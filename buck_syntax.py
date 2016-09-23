a39ba002e82015e8776d6708cad751599d631c87
Anh Tuan Khuc <anhk@uber.com>
2016-09-23 01:44:21 -0700

    Fix BUCK

--- buck_syntax.py
+++ buck_syntax.py
@@ -2,5 +2,5 @@ import sublime, sublime_plugin
 
 class SetSyntaxForBuck(sublime_plugin.EventListener):
     def on_activated(self, view):
-        if view.name() == "BUCK":
+        if view.file_name().endswith("BUCK"):
             view.set_syntax_file("Packages/Python/Python.tmLanguage")
