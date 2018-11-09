f64f38256c09dc1490e046f92c31ecfa770d749c
Anh Khuc <anh@quip.com>
2018-11-09 00:50:03 -0800

    Double copy will copy word with dot (.) character

--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 import sublime, sublime_plugin
 import re
+import time
 
 class ReplaceAllAppearances(sublime_plugin.WindowCommand):
 
@@ -84,10 +85,45 @@ class AlignColon(sublime_plugin.TextCommand):
                         self.view.erase(edit, sublime.Region(cur.begin(), cur.begin() + r))
 
 class CopyCurrentWord(sublime_plugin.TextCommand):
+    last_copy_time = None
+    last_copy_id = None
+    allowedSet = None
     def run(self, edit):
+        copy_delay = 1.2
         for region in self.view.sel():
             if region.empty():
-                sublime.set_clipboard(self.view.substr(self.view.word(region.begin())))
+                current_time = time.time()
+                current_id = self.get_copy_id(region)
+                if self.last_copy_time and self.last_copy_id == current_id and (current_time < self.last_copy_time + copy_delay):
+                    sublime.set_clipboard(self.view.substr(self.get_extended_region(region.begin())))
+                else:
+                    sublime.set_clipboard(self.view.substr(self.view.word(region.begin())))
+                self.last_copy_time = current_time
+                self.last_copy_id = current_id
+                break
+
+    def get_copy_id(self, region):
+        return "%d:%d:%d" % (self.view.id(), region.begin(), region.end())
+
+    def get_extended_region(self, pos):
+        if not self.allowedSet:
+            self.allowedSet = set()
+            for c in range(ord('a'), ord('z') + 1):
+                self.allowedSet.add(chr(c))
+            for c in range(ord('A'), ord('Z') + 1):
+                self.allowedSet.add(chr(c))
+            for c in range(ord('0'), ord('9') + 1):
+                self.allowedSet.add(chr(c))
+            self.allowedSet.add('_')
+            self.allowedSet.add('.')
+
+        r = pos
+        while r < self.view.size() and self.view.substr(r) in self.allowedSet:
+            r = r + 1
+        l = pos - 1
+        while l >= 0 and self.view.substr(l) in self.allowedSet:
+            l = l - 1
+        return sublime.Region(l + 1, r)
 
 # We need to run a text command in order to force the view to update it cursor rendering
 class ShowViewAtPosition(sublime_plugin.TextCommand):
